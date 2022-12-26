78d4a47fc129555ae9c70aa31d24e541ee6fee37
chompi <chompister@gmail.com>
2022-12-26 10:29:02 -0300

    de_linux_a_windows

---
+++ js/function.js
@@ -0,0 +1,16 @@
+
+/*
+document.getElementById("boton").onclick=function(){
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML="Estamos probando nuestro primer script js";
+}*/
+
+document.getElementById('boton').addEventListener('click',function(){
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML="Estamos probando nuestro primer evento";
+})
+
+document.getElementById("boton_fondo").onclick=function(){
+    console.log("Color de fondo");
+    document.body.style.backgroundColor='#905050';
+}
