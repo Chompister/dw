426b7fca2a48110f520b8c8b509c73d121fca45f
chompi <chompister@gmail.com>
2022-12-28 00:52:00 -0300

    funciones

--- js/function.js
+++ js/function.js
@@ -4,7 +4,7 @@ document.getElementById("boton").onclick=function(){
     console.log("Capturamos el evento click");
     document.getElementById("demo").innerHTML="Estamos probando nuestro primer script js";
 }*/
-
+/*
 document.getElementById('boton').addEventListener('click',function(){
     console.log("Hola mundo desde EventListener");
     document.getElementById("demo").innerHTML="Estamos probando nuestro primer evento";
@@ -15,3 +15,22 @@ document.getElementById("boton_fondo").onclick=function(){
     document.body.style.backgroundColor='#905050';
 }
  /* a donde sube esto? */
+
+document.getElementById('sumar').addEventListener('click',function(){
+    console.log("hola");
+    
+    let NumeroA = document.getElementById('numero1').value;
+    console.log('El valor de NumeroA es '+NumeroA);
+    let NumeroB = document.getElementById('numero2').value;
+    console.log('El valor de NumeroB es '+ NumeroB);
+
+    let resultado = sumar(parseInt(NumeroA),parseInt(NumeroB));
+    console.log('El valor del resultado es '+ resultado);
+
+    document.getElementById("resultado").innerHTML = resultado;
+    document.getElementById("ContenedorResultado").style.display = 'block';
+ })
+
+ function sumar(a,b){
+    return a + b;
+ }
